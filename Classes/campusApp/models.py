db53cc7d8da885e6c60f1306f957e0cb52bc7e67
Shaelynn Caltagirone <78838464+Taiilzmf@users.noreply.github.com>
2022-08-10 11:08:31 -0700

    Update models.py

--- Classes/campusApp/models.py
+++ Classes/campusApp/models.py
@@ -10,6 +10,13 @@ class UniversityCampus(models.Model):
     # creates model manager
     object = models.Manager()
 
+
+    def __str__(self):
+        # returns input value of the title and instructor name
+        # field as a tuple to display in the browser instead of the default titles
+        display_course = '{0.campus_name}: {0.state}'
+        return display_course.format(self)
+
     # removes added 's' that django adds to model name in browser display
     class Meta:
         verbose_name_plural = "University Campus"
